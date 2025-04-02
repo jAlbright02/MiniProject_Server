5d76855ee15f27cdc5ef61875d1eb1787aaf1568
JamesA <G00379271@atu.ie>
2025-04-02 12:56:58 +0100

    alter code for posts to be added

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const shopRoutes = require('./routes/shop');
+const socMedRoutes = require('./routes/social_media');
 app.use(express.json()) // Won't parse JSON data sent to server without this
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', shopRoutes.routes);
-
-//app.use((req, res, next) => {
-    //  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-//});
+app.use('/', socMedRoutes.routes);
 
 mongoose.set('strictQuery', true)
 mongoose.connect('mongodb://127.0.0.1:27017/db')
---
+++ routes/social_media.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const router = express.Router();
+
+const moment = require('moment');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  id: {type: String, required: true},
+  content: { type: String, required: true },
+  user: { type: String, required: true },
+  timestamp: { type: Date, default: moment(Date.now()).format('DD/MM/YYYY') },
+  likes: { type: Number, default: 0 },
+  comments: [{ 
+    user: { type: String, required: false },
+    content: { type: String, required: false },
+    timestamp: { type: Date, default: moment(Date.now()).format('DD/MM/YYYY') }
+  }],
+  images: [{ type: String }]
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+router.post('/addPost', (req, res, next) => {
+  const { id, content, user, images } = req.body;
+
+  new Post({ 
+    id: id,
+    content: content, 
+    user: user, 
+    images: images || []
+  })
+    .save()
+    .then(result => {
+      console.log('Post saved to database');
+      res.json({ success: true });
+    })
+    .catch(err => {
+      console.log('Failed to add post: ' + err);
+      res.json({ success: false, theError: err });
+    });
+});
+
+
+router.post('/', (req, res, next) => {
+  Post.find()
+    .then(posts => {
+      res.json({ success: true, posts: posts });
+    })
+    .catch(err => {
+      console.log('Failed to find posts: ' + err);
+      res.json({ success: false, theError: err });
+    });
+});
+
+router.post('/getSpecificPost', (req, res, next) => {
+  const { id } = req.body;
+  Post.find({ _id: id })
+    .then(posts => {
+      res.json({ success: true, post: posts[0] });
+    })
+    .catch(err => {
+      console.log('Failed to find post: ' + err);
+      res.json({ success: false, theError: err });
+    });
+});
+
+router.post('/deleteSpecificPost', (req, res, next) => {
+  const { id } = req.body;
+  Post.findOneAndRemove({ _id: id })
+    .then(resp => {
+      res.json({ success: true });
+    })
+    .catch(err => {
+      console.log('Failed to delete post: ' + err);
+      res.send('Post not found');
+    });
+});
+
+// router.post('/updateSpecificPost', (req, res, next) => {
+//   const { id, content, likes, comments } = req.body;
+
+//   Post.findById(id)
+//     .then(post => {
+//       if (post) {
+//         post.content = content || post.content;
+//         post.likes = likes || post.likes;
+//         post.comments = comments || post.comments;
+
+//         post.save()
+//           .then(updatedPost => {
+//             res.json({ success: true, post: updatedPost });
+//           })
+//           .catch(err => {
+//             res.json({ success: false, theError: err });
+//           });
+//       } else {
+//         res.json({ success: false, message: 'Post not found' });
+//       }
+//     })
+//     .catch(err => {
+//       console.log('Error updating post: ' + err);
+//       res.json({ success: false, theError: err });
+//     });
+// });
+
+module.exports = router;
